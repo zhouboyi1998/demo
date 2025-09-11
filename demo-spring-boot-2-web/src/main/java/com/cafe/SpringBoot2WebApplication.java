2b7c62810b1eb21ebc692a40c76e31dc263ce3a9
zhouboyi <zby02126265@alibaba-inc.com>
2025-09-11 11:03:38 +0800

    build(module): 新增demo-spring-boot-2-web模块

---
+++ demo-spring-boot-2-web/src/main/java/com/cafe/SpringBoot2WebApplication.java
@@ -0,0 +1,19 @@
+package com.cafe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Project: demo
+ * @Package: com.cafe
+ * @Author: zhouboyi
+ * @Date: 2025/9/11 10:40
+ * @Description:
+ */
+@SpringBootApplication
+public class SpringBoot2WebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot2WebApplication.class, args);
+    }
+}
